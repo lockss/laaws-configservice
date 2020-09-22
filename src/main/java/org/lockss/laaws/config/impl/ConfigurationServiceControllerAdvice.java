afa2210ef05a727e9407e938a00720c2f153aa65
Daniel Vargas <dlvargas@stanford.edu>
2020-09-22 14:32:52 -0700

    Moved service controller advice to BaseSpringBootApplication

--- src/main/java/org/lockss/laaws/config/impl/ConfigurationServiceControllerAdvice.java
+++
@@ -1,9 +0,0 @@
-package org.lockss.laaws.config.impl;
-
-import org.lockss.spring.error.SpringControllerAdvice;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class ConfigurationServiceControllerAdvice extends SpringControllerAdvice {
-  // Intentionally left blank
-}
