7f6f3cc32d125adc18667763c9062545c001e052
Daniel Vargas <dlvargas@stanford.edu>
2024-01-30 07:01:46 -0800

    Removed Swagger2 annotation

--- src/main/java/org/lockss/laaws/config/ConfigApplication.java
+++ src/main/java/org/lockss/laaws/config/ConfigApplication.java
@@ -41,13 +41,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * The Spring-Boot application.
  */
 @SpringBootApplication()
-@EnableSwagger2
 public class ConfigApplication extends BaseSpringBootApplication
 	implements CommandLineRunner {
   private static final Logger logger =
