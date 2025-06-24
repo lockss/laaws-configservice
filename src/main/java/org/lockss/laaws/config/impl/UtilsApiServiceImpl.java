92cae1a2c38c527eb016782bd3b789e458f61754
Daniel Vargas <dlvargas@stanford.edu>
2025-06-24 13:48:58 -0700

    WIP: Improvements to CDX record endpoints

---
+++ src/main/java/org/lockss/laaws/config/impl/UtilsApiServiceImpl.java
@@ -0,0 +1,73 @@
+/*
+
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+package org.lockss.laaws.config.impl;
+
+import org.lockss.app.LockssDaemon;
+import org.lockss.laaws.config.api.UtilsApiDelegate;
+import org.lockss.log.L4JLogger;
+import org.lockss.plugin.PluginManager;
+import org.lockss.spring.base.BaseSpringApiServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UtilsApiServiceImpl extends BaseSpringApiServiceImpl
+    implements UtilsApiDelegate {
+
+  private static L4JLogger log = L4JLogger.getLogger();
+
+  @Override
+  public ResponseEntity<List<String>> normalizeUrl(String url) {
+    LockssDaemon theDaemon = LockssDaemon.getLockssDaemon();
+    PluginManager pluginMgr = theDaemon.getPluginManager();
+
+    if (!pluginMgr.areAusStarted()) {
+      String msg = "AUs still starting";
+      return new ResponseEntity<>(null, HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
+    try {
+      Set<String> normalizedUrls = pluginMgr.normalizeUrl(url);
+      return new ResponseEntity<>(new ArrayList<>(normalizedUrls), HttpStatus.OK);
+    } catch (MalformedURLException e) {
+      String msg = "Malformed URL: " + url;
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
+}
---
+++ src/test/java/org/lockss/laaws/config/impl/TestUtilsApiServiceImpl.java
@@ -0,0 +1,152 @@
+/*
+Copyright (c) 2000-2025, Board of Trustees of Leland Stanford Jr. University
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+package org.lockss.laaws.config.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.lockss.laaws.config.ConfigApplication;
+import org.lockss.log.L4JLogger;
+import org.lockss.spring.test.SpringLockssTestCase4;
+import org.lockss.util.rest.RestUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    classes = {ConfigApplication.class},
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestUtilsApiServiceImpl extends SpringLockssTestCase4 {
+  private static final L4JLogger log = L4JLogger.getLogger();
+
+  // The port that Tomcat is using during this test.
+  @LocalServerPort
+  private int port;
+
+  // The application Context used to specify the command line arguments to be
+  // used for the tests.
+  @Autowired
+  ApplicationContext appCtx;
+
+  @Before
+  public void setUpBeforeEachTest() throws IOException {
+    log.debug2("port = {}", port);
+
+    // Set up the temporary directory where the test data will reside.
+    setUpTempDirectory(TestUtilsApiServiceImpl.class.getCanonicalName());
+
+    // Set up the UI port.
+    setUpUiPort(UI_PORT_CONFIGURATION_TEMPLATE, UI_PORT_CONFIGURATION_FILE);
+
+    log.debug2("Done");
+  }
+
+  @Test
+  public void runAuthenticatedTests() throws Exception {
+    log.debug2("Invoked");
+
+    // Specify the command line parameters to be used for the tests.
+    List<String> cmdLineArgs = getCommandLineArguments();
+    cmdLineArgs.add("-p");
+    cmdLineArgs.add("test/config/testAuthOff.txt");
+
+    CommandLineRunner runner = appCtx.getBean(CommandLineRunner.class);
+    runner.run(cmdLineArgs.toArray(new String[cmdLineArgs.size()]));
+
+    assertNormalizedURL("test", "test");
+    assertNormalizedURL("HTTP://www.lockss.org", "http://www.lockss.org/");
+  }
+
+  private void assertNormalizedURL(String url, String expectedNormalizedUrl) {
+    String restEndpoint = getTestUrlTemplate("/utils/normalizeUrl?url=" + url);
+    ResponseEntity<List> response =
+        RestUtil.getRestTemplate().exchange(restEndpoint, HttpMethod.GET, (HttpEntity)null, List.class, new Object[0]);
+
+    HttpStatusCode statusCode = response.getStatusCode();
+    HttpStatus status = HttpStatus.resolve(statusCode.value());
+    assertEquals(HttpStatus.OK, status);
+    List<String> actualNormalizedUrl = response.getBody();
+
+    assertEquals(expectedNormalizedUrl, actualNormalizedUrl.get(0));
+  }
+
+  /**
+   * Provides the standard command line arguments to start the server.
+   *
+   * @return a List<String> with the command line arguments.
+   */
+  private List<String> getCommandLineArguments() {
+    log.debug2("Invoked");
+
+    List<String> cmdLineArgs = new ArrayList<String>();
+    cmdLineArgs.add("-p");
+    cmdLineArgs.add(getPlatformDiskSpaceConfigPath());
+
+    File folder =
+        new File(new File(new File(getTempDirPath()), "tdbxml"), "prod");
+    log.trace("folder = {}", folder);
+
+    cmdLineArgs.add("-x");
+    cmdLineArgs.add(folder.getAbsolutePath());
+    cmdLineArgs.add("-p");
+    cmdLineArgs.add(getUiPortConfigFile().getAbsolutePath());
+    cmdLineArgs.add("-p");
+    cmdLineArgs.add("test/config/lockss.txt");
+    cmdLineArgs.add("-p");
+    cmdLineArgs.add("test/config/lockss.opt");
+
+    log.debug2("cmdLineArgs = {}", cmdLineArgs);
+    return cmdLineArgs;
+  }
+
+  /**
+   * Provides the URL template to be tested.
+   *
+   * @param pathAndQueryParams
+   *          A String with the path and query parameters of the URL template to
+   *          be tested.
+   * @return a String with the URL template to be tested.
+   */
+  private String getTestUrlTemplate(String pathAndQueryParams) {
+    return "http://localhost:" + port + pathAndQueryParams;
+  }
+}
